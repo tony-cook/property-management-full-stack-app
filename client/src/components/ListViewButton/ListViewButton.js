c6a19134f46c399729b51d2c8f4e5dfc80df2da3
Tony Cook <tony_cook@outlook.com>
2022-03-22 00:03:04 +1300

    re-doing last commit

---
+++ client/src/components/ListViewButton/ListViewButton.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+
+
+
+function ListViewButton() {
+  const ListViewButton = styled(Button)({
+    boxShadow: 'none',
+    textTransform: 'none',
+    fontSize: 16,
+    color: '#bf163d',
+    padding: '6px 12px',
+    border: '1px solid',
+    lineHeight: 1.5,
+    backgroundColor: '#ffffff',
+    borderColor: '#bf163d',
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:hover': {
+      backgroundColor: '#ffffff',
+      borderColor: '#bf163d',
+      boxShadow: 'none',
+    },
+    '&:active': {
+      boxShadow: 'none',
+      backgroundColor: '#ffffff',
+      borderColor: '#bf163d',
+    }
+  });
+
+  return (
+    <div>
+      <ListViewButton variant="contained" disableRipple>
+        <img src="list-view-icon.png" alt="icon.png" />
+        List View
+      </ListViewButton>
+    </div>
+  )
+}
+
+export default ListViewButton
---
+++ client/src/components/SortSelect/SortSelect.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import { styled } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import NativeSelect from '@mui/material/NativeSelect';
+
+function SortSelect() {
+    const BootstrapInput = styled(InputBase)(({ theme }) => ({
+        'label + &': {
+          marginTop: theme.spacing(3),
+        },
+        '& .MuiInputBase-input': {
+          borderRadius: 4,
+          position: 'relative',
+          backgroundColor: '#ffffff',
+          border: '1px solid #ced4da',
+          fontSize: 16,
+          padding: '10px 26px 10px 12px',
+          width: '400px',
+          marginTop: '-10px',
+          transition: theme.transitions.create(['border-color', 'box-shadow']),
+          // Use the system font instead of the default Roboto font.
+          fontFamily: [
+            '-apple-system',
+            'BlinkMacSystemFont',
+            '"Segoe UI"',
+            'Roboto',
+            '"Helvetica Neue"',
+            'Arial',
+            'sans-serif',
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"',
+          ].join(','),
+          '&:focus': {
+            borderRadius: 4,
+            borderColor: '#bf163d',
+            boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
+          },
+        },
+      }));
+
+    const [sort, setSort] = React.useState('');
+
+    const handleChange = (event) => {
+        setSort(event.target.value);
+      };
+
+  return (
+    <div>
+        <FormControl sx={{ m: 1 }} variant="standard">
+        <NativeSelect
+          id="demo-customized-select-native"
+          value={sort}
+          onChange={handleChange}
+          input={<BootstrapInput />}
+        >
+          <option value={10}>Sort: Featured Listings</option>
+          <option value={20}>Lowest Price</option>
+          <option value={30}>Highest Price</option>
+          <option value={40}>Available Date</option>
+        </NativeSelect>
+      </FormControl>
+    </div>
+  )
+}
+
+export default SortSelect
